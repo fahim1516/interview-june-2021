0e893e547685845328e4cd14ab9736b688a3104a
fahim1516 <74896056+fahim1516@users.noreply.github.com>
2021-06-22 13:08:07 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,34 @@
 package exam;
 
 public class ConvertToBinary {
+  
+  
+  
+  
+  
+  
 
   public String toBinary(final int n) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+  
+
+    if (n < 0)
+      return "Invalid number. Please input positive number.";
+    if(n==0)
+        return "0";
+
+    int[] binaryNum = new int[1000];
+    int i = 0;
+    int temp = n;
+    while (temp > 0)
+      {
+	binaryNum[i] = temp % 2;
+	temp = temp / 2;
+	i++;
+      }
+    StringBuilder sb = new StringBuilder ();
+    for (int j = i - 1; j >= 0; j--)
+      sb.append (binaryNum[j]);
+    return sb.toString ();
   }
 }
